cdee1c5f1cc7fbf0ec0363f98c9e6d1432c47167
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-08-16 16:00:30 +0200

    Verify tailwind in ui/button

--- src/components/ui/button/Button.vue
+++ src/components/ui/button/Button.vue
@@ -20,6 +20,7 @@ const props = withDefaults(defineProps<Props>(), {
 <template>
   <Primitive
     data-slot="button"
+    class="text-red-500"
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
