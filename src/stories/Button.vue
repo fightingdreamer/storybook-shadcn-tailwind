b7893b84f59dfc0e7bb987e0d0c7b4a8350127c0
Patryk Stępień <fighting.dreamer.0@gmail.com>
2025-08-16 15:59:23 +0200

    Add ui/button to storybook

--- src/stories/Button.vue
+++ src/stories/Button.vue
@@ -3,9 +3,9 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed } from "vue";
 
-import './button.css';
+import "./button.css";
 
 const props = withDefaults(
   defineProps<{
@@ -20,24 +20,24 @@ const props = withDefaults(
     /**
      * size of the button
      */
-    size?: 'small' | 'medium' | 'large';
+    size?: "small" | "medium" | "large";
     /**
      * background color of the button
      */
     backgroundColor?: string;
   }>(),
-  { primary: false }
+  { primary: false },
 );
 
 const emit = defineEmits<{
-  (e: 'click', id: number): void;
+  (e: "click", id: number): void;
 }>();
 
 const classes = computed(() => ({
-  'storybook-button': true,
-  'storybook-button--primary': props.primary,
-  'storybook-button--secondary': !props.primary,
-  [`storybook-button--${props.size || 'medium'}`]: true,
+  "storybook-button": true,
+  "storybook-button--primary": props.primary,
+  "storybook-button--secondary": !props.primary,
+  [`storybook-button--${props.size || "medium"}`]: true,
 }));
 
 const style = computed(() => ({
@@ -45,6 +45,6 @@ const style = computed(() => ({
 }));
 
 const onClick = () => {
-  emit('click', 1);
+  emit("click", 1);
 };
 </script>
